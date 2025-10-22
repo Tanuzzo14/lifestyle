fb7a786a2b684818d01358dda8ec9290f10d07c9
Gaetano Smario <gaetano.smario@top-network.it>
2025-10-22 15:02:13 +0200

    Versione funzionante

--- test_auth_base_user.js
+++ test_auth_base_user.js
@@ -1,7 +1,7 @@
 // Integration test for BASE_USER functionality using auth.js
-import { Auth } from './back-end/auth.js';
+import { Auth } from './auth.js';
 
-const API_URL = 'http://localhost:8000/back-end/api.php';
+const API_URL = 'http://localhost:8000/api.php';
 
 async function apiCall(method, params = {}) {
   try {
